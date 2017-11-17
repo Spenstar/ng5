c30c468df6e33c835c960179d8b06343f86fec4b
Spencer Memmott <skmemmott@familysearch.org>
2017-11-17 08:22:29 -0700

    (feat): add goals and animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,7 +17,9 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
